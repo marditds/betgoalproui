018fbe91864f7470dbe875874ba8ddbe959c327a
marditds <marditds@gmail.com>
2023-04-10 22:35:04 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import { NavMenu } from './components/NavMenu'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavMenu />
+      <Row>
+        <Col></Col>
+        <Col><h1>Hakop</h1></Col>
+        <Col></Col>
+      </Row>
+
+
     </div>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, Navbar, Form, Nav } from 'react-bootstrap'
+
+export const NavMenu = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">ToTo ETH</Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Form className="ms-lg-auto w-50">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className=""
+                                aria-label="Search"
+                            /></Form>
+                        <Nav className='ms-lg-auto text-end'>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#matches">Matches</Nav.Link>
+                            <Nav.Link href="#connectWallet">Connect Wallet</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
