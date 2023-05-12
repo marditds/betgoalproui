4f055c6fd9de5118e2e148ebb240638bc812d35e
marditds <marditds@gmail.com>
2023-05-12 16:35:01 -0700

    stack

--- src/components/League/Other_Leagues.js
+++ src/components/League/Other_Leagues.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col } from 'react-bootstrap';
+import { Stack, Row, Col } from 'react-bootstrap';
 import './Other_Leagues.css'
 
 export const Other_Leagues = ({ leagues }) => {
     return (
         <Col lg={5}>
             <h4>Other Leagues</h4>
-            {leagues.map((tournoment, i) => {
-                return (
-                    <Link key={i} className='d-flex other--leagues--link' to={`/leagues/${tournoment.alias}/picks`}>
-                        {tournoment.name}
-                    </Link>
-                )
-            }
-            )}
+            <Stack className='other--leagues--div'>
+                {leagues.map((tournoment, i) => {
+                    return (
+                        <div className='other--leagues--link--div' key={i}>
+                            <Link className='d-flex other--leagues--link' to={`/leagues/${tournoment.alias}/picks`}>
+                                {tournoment.name}
+                            </Link>
+                        </div>
+                    )
+                }
+                )}
+            </Stack>
         </Col>
     )
 }
