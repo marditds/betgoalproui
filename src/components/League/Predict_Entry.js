9d2a86428a089da63e5b8cf355ac7f9ac56a2808
marditds <marditds@gmail.com>
2023-05-13 23:01:18 -0700

    modal

--- src/components/League/Predict_Entry.js
+++ src/components/League/Predict_Entry.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Row, Col, Form, Button } from 'react-bootstrap'
+import { Row, Col, Form, Button, Modal } from 'react-bootstrap'
 import { Counter } from './Counter';
 import './Predict_Entry.css'
 
@@ -7,8 +7,14 @@ import './Predict_Entry.css'
 
 export const Predict_Entry = ({ league }) => {
 
+    const [isCompleted, setIsCompleted] = useState(false);
+    const [show, setShow] = useState(true);
     const [value, setValue] = useState(0);
     const [resetKey, setResetKey] = useState(0);
+
+    const handleSubmit = () => {
+        setIsCompleted((preVal) => true);
+    }
 
     const handleReset = () => {
         console.log('reset clicked!');
@@ -19,154 +25,179 @@ export const Predict_Entry = ({ league }) => {
     console.log('PREDITCT');
     return (
         <Col>
-            <h2>Make Your Picks for {league.name}</h2>
-            <div className="predict--entry--div">
-                <Form className='predict--entry--form'>
+            {!isCompleted ?
+                <>
+                    <h2>Make Your Picks for {league.name}</h2>
+                    <div className="predict--entry--div">
+                        <Form className='predict--entry--form'>
 
-                    <Form.Group className='myFG'>
-                        <Row className='match--row pt-4 pb-4'>
-                            <Col className='d-flex justify-content-end align-items-center '>
-                                <Form.Label className='my-0 me-3'>
-                                    {league.clubs[1].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[1].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[1].name
-                                    }
-                                </Form.Label>
-                                {/* <Form.Control
+                            <Form.Group className='myFG'>
+                                <Row className='match--row pt-4 pb-4'>
+                                    <Col className='d-flex justify-content-end align-items-center '>
+                                        <Form.Label className='my-0 me-3'>
+                                            {league.clubs[1].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[1].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[1].name
+                                            }
+                                        </Form.Label>
+                                        {/* <Form.Control
                                     type='number'
                                     value={value}
                                     className='team--points'
                                     readOnly
                                 /> */}
-                                <Counter key={`${value}-${resetKey}`} value={value} />
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
 
-                            </Col>
-
-
-                            <Col className='d-flex justify-content-start align-items-center'>
-                                <Counter key={`${value}-${resetKey}`} value={value} />
-
-                                <Form.Label className='my-0 ms-3'>
-                                    {league.clubs[7].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[7].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[7].name
-                                    }
-                                </Form.Label>
-                            </Col >
-                        </Row>
-                    </Form.Group>
+                                    </Col>
 
 
+                                    <Col className='d-flex justify-content-start align-items-center'>
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
+
+                                        <Form.Label className='my-0 ms-3'>
+                                            {league.clubs[7].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[7].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[7].name
+                                            }
+                                        </Form.Label>
+                                    </Col >
+                                </Row>
+                            </Form.Group>
 
 
-                    <Form.Group className='myFG'>
-
-                        <Row className='match--row pt-4 pb-4'>
-                            <Col className='d-flex justify-content-end align-items-center'>
-
-                                <Form.Label className='my-0 me-3'>
-                                    {league.clubs[3].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[3].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[3].name
-                                    }
-                                </Form.Label>
-                                <Counter key={`${value}-${resetKey}`} value={value} />
-                            </Col>
-
-                            <Col className='d-flex justify-content-start align-items-center'>
-
-                                <Counter key={`${value}-${resetKey}`} value={value} />
-
-                                <Form.Label className='my-0 ms-3'>
-                                    {league.clubs[6].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[6].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[6].name
-                                    }
-                                </Form.Label>
-                            </Col>
-
-                        </Row>
-                    </Form.Group>
 
 
-                    <Form.Group className='myFG'>
+                            <Form.Group className='myFG'>
 
-                        <Row className='match--row pt-4 pb-4'>
-                            <Col className='d-flex justify-content-end align-items-center'>
+                                <Row className='match--row pt-4 pb-4'>
+                                    <Col className='d-flex justify-content-end align-items-center'>
 
-                                <Form.Label className='my-0 me-3'>
-                                    {league.clubs[5].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[5].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[5].name
-                                    }
-                                </Form.Label>
-                                <Counter key={`${value}-${resetKey}`} value={value} />
+                                        <Form.Label className='my-0 me-3'>
+                                            {league.clubs[3].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[3].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[3].name
+                                            }
+                                        </Form.Label>
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
+                                    </Col>
+
+                                    <Col className='d-flex justify-content-start align-items-center'>
+
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
+
+                                        <Form.Label className='my-0 ms-3'>
+                                            {league.clubs[6].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[6].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[6].name
+                                            }
+                                        </Form.Label>
+                                    </Col>
+
+                                </Row>
+                            </Form.Group>
 
 
-                            </Col>
+                            <Form.Group className='myFG'>
 
-                            <Col className='d-flex justify-content-start align-items-center'>
+                                <Row className='match--row pt-4 pb-4'>
+                                    <Col className='d-flex justify-content-end align-items-center'>
 
-                                <Counter key={`${value}-${resetKey}`} value={value} />
+                                        <Form.Label className='my-0 me-3'>
+                                            {league.clubs[5].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[5].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[5].name
+                                            }
+                                        </Form.Label>
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
 
 
-                                <p className='my-0 ms-3'>
-                                    {league.clubs[4].name.split(" ").slice(0, 2).length > 1
-                                        ?
-                                        league.clubs[4].name.split(" ").slice(0, 2).map((team, i) =>
-                                            <React.Fragment key={i}>
-                                                {team.slice(0, 3)}
-                                                {" "}
-                                            </React.Fragment>
-                                        ) :
-                                        league.clubs[4].name
-                                    }
-                                </p>
+                                    </Col>
 
-                            </Col>
+                                    <Col className='d-flex justify-content-start align-items-center'>
 
-                        </Row>
-                    </Form.Group>
-                    <div className='d-flex justify-content-end mb-4 me-3'>
-                        <Button className="picks--btn--rst me-3 ms-3 ms-sm-0" onClick={handleReset}>
-                            Reset
-                        </Button>
-                        <Button type="submit" className="picks--btn--smbt">
-                            Submit
-                        </Button>
+                                        <Counter key={`${value}-${resetKey}`} value={value} />
+
+
+                                        <p className='my-0 ms-3'>
+                                            {league.clubs[4].name.split(" ").slice(0, 2).length > 1
+                                                ?
+                                                league.clubs[4].name.split(" ").slice(0, 2).map((team, i) =>
+                                                    <React.Fragment key={i}>
+                                                        {team.slice(0, 3)}
+                                                        {" "}
+                                                    </React.Fragment>
+                                                ) :
+                                                league.clubs[4].name
+                                            }
+                                        </p>
+
+                                    </Col>
+
+                                </Row>
+                            </Form.Group>
+                            <div className='d-flex justify-content-end mb-4 me-3'>
+                                <Button className="picks--btn--rst me-3 ms-3 ms-sm-0" onClick={handleReset}>
+                                    Reset
+                                </Button>
+                                <Button type="submit" className="picks--btn--smbt" onClick={handleSubmit}>
+                                    Submit
+                                </Button>
+                            </div>
+                        </Form>
                     </div>
-                </Form>
-            </div>
-        </Col >
+                </>
+                :
+                <>
+                    <Modal
+                        show={show}
+                        onHide={() => setShow(false)}
+                        animation={false}
+                        centered
+                        className='modal--content'
+                    >
+                        <Modal.Header className='modal--header'>
+                            <Modal.Title>You're all done!</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body className='modal--body'>
+                            <p>Please check back later for more matches.</p>
+                            <div className='d-flex justify-content-end'>
+                                <Button onClick={() => setShow(false)}>Close</Button>
+                            </div>
+                        </Modal.Body>
+                    </Modal>
+                    <h2>No more matches. Please check back later.</h2>
+                </>
+            }
+        </Col>
     )
 }
