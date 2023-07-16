e72eef525d317691cd8861aa8f97318ace258937
marditds <marditds@gmail.com>
2023-07-15 21:20:47 -0700

    disclaimer

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -7,7 +7,7 @@ import { GuestLogin } from './GuestLogin/GuestLogin';
 export const WelcomePage = ({ connectWallet, guestName, handleGuestLogin, updateGuestName, guestNameMsg }) => {
     return (
         <Container className='welcome--body'>
-            <span className='w-100'>
+            <span className='w-100 welcome--btns'>
                 <ConnectWallet
                     connectWallet={connectWallet} />
                 <GuestLogin
@@ -16,9 +16,13 @@ export const WelcomePage = ({ connectWallet, guestName, handleGuestLogin, update
                     updateGuestName={updateGuestName}
                     guestNameMsg={guestNameMsg} />
 
-                <div className='text-center'>Hello</div>
+                {/* <div className='position-absolute' style={{ marginTop: '200px' }}>Hello</div> */}
 
             </span>
+            <div className='position-absolute mx-4' style={{ marginTop: '650px' }}>
+                BetGoalPro application uses the Hedera™ network. Hedera” is a trademark of Hedera Hashgraph, LLC. BetGoalPro is not affiliated with, sponsored, or endorsed by Hedera Hashgraph, LLC. All rights reserved.
+            </div>
+
         </Container>
     )
 }
